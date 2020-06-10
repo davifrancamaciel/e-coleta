23d5023aa4879459f3d9881f8ed7c232e146089c
Davi França Maciel <davifrancamaciel@hotmail.com>
2020-06-10 20:19:55 -0300

    first commit

---
+++ backend/src/constants/config.ts
@@ -0,0 +1,2 @@
+export const BASE_PORT = 3333
+export const BASE_URL = `http://localhost:${BASE_PORT}`
---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
+
+import routes from './routes'
+import { BASE_PORT } from './constants/config'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.listen(BASE_PORT, () => {
+  console.log(`[*] Server running on port: ${BASE_PORT}`)
+})
